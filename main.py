9c41413363b90358c63fde59de8f1c2ede9a89e5
HyXFR <74453676+HyXFR@users.noreply.github.com>
2021-04-04 14:47:44 +0200

    Still don't work... But in english

--- main.py
+++ main.py
@@ -14,9 +14,11 @@ for key, value in yaml_content.items():
         _citd = value
     elif key == "guild_id_to_send":
         _gits = value
+    elif key == "prefix":
+        _prefix = value
 
 # Bot Init
-bot = commands.Bot(command_prefix="m")
+bot = commands.Bot(command_prefix=_prefix)
 bot.remove_command("help")
 token_openned = []
 
@@ -33,19 +35,20 @@ async def on_message(message):
         return
     
     if not message.guild:
-        ctx = await bot.get_context(message)
+        uctx = await bot.get_context(message)
+        print(str(dmed_open))
         guild = bot.get_guild(_gits)
         channel = discord.utils.get(guild.text_channels, name="mailbot")
         if message.author.id not in token_openned:
             token_openned.append(message.author.id)
             print("[MailBot User] Received new ticket id " + str(message.author.id) + " Contain : " + message.content)
-            await ctx.send("You have successfull create a ticket ! User `close` to close it !")
+            await uctx.send("You have successfull create a ticket ! User `close` to close it !")
             await channel.send(f"[{message.author.id}] has create a ticket, content : " + message.content)
         else:
             if message.content == "close":
                 token_openned.remove(message.author.id)
                 print("[MailBot User] User " + str(message.author.id) + " closed his ticket")
-                await ctx.send("Your ticket are successfull closed !")
+                await uctx.send("Your ticket are successfull closed !")
                 await channel.send(f"[{message.author.id}] has closed his ticket !")
             else:
                 print("[MailBot User] User " + str(message.author.id) + " has sent " + message.content)
@@ -54,27 +57,23 @@ async def on_message(message):
 
 @bot.command(aliases=["r"])
 async def reply(ctx, id, *message):
-    id = "".join(id)
-    id = int(id)
-    print(str(id))
-    message = "".join(message)
-    message = str(message)
-    print(str(message))
+    message = " ".join(message)
+    id = int("".join(id))
     if id not in token_openned:
-        print("[MailBot Staff] " + str(ctx.author.id) + " a essayé de réplier au ticket id " + str(id) + " avec pour message " + message)
-        msg = ctx.send(ctx.author.name + ", Le ticket est fermé ou n'existe pas.")
+        print("[MailBot Staff] " + str(ctx.author.id) + " tryed to reply the ticket " + str(id) + " with message " + str(message))
+        msg = ctx.send(ctx.author.name + ", This ticket don't exist.")
         await asyncio.sleep(5)
         await msg.delete()
     
     else:
         user = bot.get_user(id)
         if message == "close":
-            print("[MailBot Staff] " + str(ctx.author.id) + " a fermé le ticket de l'utilisateur " + str(id))
+            print("[MailBot Staff] " + str(ctx.author.id) + " has close the ticket id " + str(id))
             token_openned.remove(id)
-            await bot.create_dm(user, "Votre ticket a été fermé ! Tout message en commenceras un nouveau !")
+            await uctx.send("Your ticket is close. All messages will start a new !")
         else:
-            print("[MailBot Staff] " + str(ctx.author.id) + " a envoyé une réponse a l'utilisateur " + str(id) + " : " + message)
-            await bot.create_dm(user, "[" + ctx.author.name + "] " + message)
+            print("[MailBot Staff] " + str(ctx.author.id) + " has sent a respond tu user " + str(id) + " : " + str(message))
+            await uctx.send("[" + ctx.author.name + "] " + message)
             
 
 bot.run(_token)
